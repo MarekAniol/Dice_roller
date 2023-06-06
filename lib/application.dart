4ed4fd0d6617d87cf251a474c778cc7a95ce5fd6
MarekAniol <marekaniol1@gmail.com>
2023-06-06 09:05:37 +0200

    add title const to app_strings.dart file

--- lib/application.dart
+++ lib/application.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:roll_dice/constants/app_strings.dart';
 import 'package:roll_dice/providers/visibility_provider.dart';
 import 'package:roll_dice/widgets/dice_screen_container.dart';
 
@@ -9,7 +10,7 @@ class RollDiceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Roll Dice',
+      title: AppStrings.title,
       home: ChangeNotifierProvider(
         create: (context) => VisibilityProvider(),
         child: const Scaffold(
--- lib/constants/app_strings.dart
+++ lib/constants/app_strings.dart
@@ -1,3 +1,4 @@
 abstract class AppStrings {
   static const String tapTheDiceText = 'Tap the dice to roll it';
+  static const String title = 'Roll Dice';
 }
